16eb9a578f46ac2b9cb5ca6abfa0900823f688d8
Evandro Firmino <efirminov@gmail.com>
2021-12-08 22:52:19 -0300

    fundamento

---
+++ curso-java/Fundamentos/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package fundamentos;
+
+/**
+ * @author Evandro Firmino
+ *
+ */
+public class PrimeiroPrograma {
+	
+	public static void main(String[] args) {
+		System.out.println("Olá, Mundo");
+	}
+
+}
