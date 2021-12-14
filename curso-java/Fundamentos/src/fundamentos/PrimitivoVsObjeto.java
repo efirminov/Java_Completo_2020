7114860f0cf5a36d7922f6fdf1c48b309befd865
Evandro Firmino <efirminov@gmail.com>
2021-12-13 22:13:11 -0300

    fundamentos

---
+++ curso-java/Fundamentos/src/fundamentos/PrimitivoVsObjeto.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+public class PrimitivoVsObjeto {
+	public static void main(String[] args) {
+		String s = "Texo";
+		s.toUpperCase();
+		
+		//Wrappers sçao a versão objeto dos tipo Primitivo
+		int a = 123;
+		System.out.println(a);
+		
+	}
+
+}
---
+++ curso-java/desafio/src/desafio/DesafioConversao.java
@@ -0,0 +1,32 @@
+package desafio;
+
+import java.util.Scanner;
+
+public class DesafioConversao {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Digite o seu primeiro salário: ");
+		String primeiro = entrada.next().replace(",", ".");
+		
+		System.out.print("Digite o seu segundo salário: ");
+		String segundo = entrada.next().replace(",", ".");
+		
+		System.out.print("Digite a sue ultimo salário: ");
+		String terceiro = entrada.next().replace(",", ".");
+		
+		double num1 = Double.parseDouble(primeiro);
+		double num2 = Double.parseDouble(segundo);
+		double num3 = Double.parseDouble(terceiro);
+		
+		double media = (num1 + num2 + num3) / 3;
+		
+		System.out.println("Média é: " + media);
+		
+		entrada.close();
+
+	}
+
+}
